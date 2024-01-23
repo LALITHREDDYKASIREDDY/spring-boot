4b8c1d349373754bc7657359e95eb486a0cac871
LALITHREDDYKASIREDDY <112109560+LALITHREDDYKASIREDDY@users.noreply.github.com>
2024-01-23 23:10:01 +0530

    springbookt
    implemented unidirectional relationship between book and publisher

---
+++ uni/target/classes/data.sql
@@ -0,0 +1,5 @@
+insert into book (name,imageUrl) values ('game of thrones','something1.jpg');
+insert into book (name,imageUrl) values ('lost','something2.jpg');
+
+insert into book (name,imageUrl) values ('dark','something3.jpg');
+insert into book (name,imageUrl) values ('death','something4.jpg');
---
+++ uni/target/classes/schema.sql
@@ -0,0 +1,7 @@
+
+create table book(
+    id int primary key auto_increment,
+    name varchar(255),
+    imageUrl varchar(255)
+);
+
